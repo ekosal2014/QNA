c0141c694c940d05dddd1562db960b1bea54b77c
ekosal2014 <ekosal2014@gmail.com>
2018-03-15 15:19:47 +0900

    dfgdsg

--- Q_AND_A/src/main/java/Q_AND_A/kh/com/mappers/UserMapper.java
+++ Q_AND_A/src/main/java/Q_AND_A/kh/com/mappers/UserMapper.java
@@ -6,5 +6,10 @@ import Q_AND_A.kh.com.domains.UserInfo;
 
 public interface UserMapper {
 	
+	/**
+	 * 
+	 * @param  strName
+	 * @return 
+	 */
 	public UserInfo loadingUserByName(@Param("strName") String strName);
 }
