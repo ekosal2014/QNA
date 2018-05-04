90c3fbfee8fc372ab33cc15a90a6cdebe65c5cdb
ekosal2014 <ekosal2014@gmail.com>
2018-05-04 13:20:34 +0900

    asfasf

--- Q_AND_A/src/main/java/Q_AND_A/kh/com/configuration/AjaxAuthenticationSuccessHandler.java
+++ Q_AND_A/src/main/java/Q_AND_A/kh/com/configuration/AjaxAuthenticationSuccessHandler.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
